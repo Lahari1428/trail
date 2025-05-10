7b73c1de2c1c19ab0ea1cc1cf5f27bbc5628e118
Lahari <lepakshilahari1428@gmail.com>
2025-05-10 13:54:14 +0530

    simple_addition

---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum=0;
+	printf("enter the a value:");
+	scanf("%d",&a);
+	printf("enter the b value:");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("the sum value is:%d\n",sum);
+	return 0;
+}
