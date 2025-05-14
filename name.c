d728ddb9328baad2701fd0287fe60f644f2c9912
Lahari <lepakshilahari1428@gmail.com>
2025-05-14 15:43:44 +0530

    simpleNamesof2people

---
+++ name.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int i=0;
+	printf("Enter the number:");
+	scanf("%d",&i);
+	if(i==0)
+		printf("Laari is a Waste_fellow or Devil\n");
+	else
+		printf("shashi is a Waste_fellow or Devil\n");
+	return 0;
+}
